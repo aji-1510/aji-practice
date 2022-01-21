66cf2c767b30e497a6a94ea902663c3f6a984bcb
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-01-21 18:00:31 +0530

    Add files via upload

---
+++ ss/ss/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            float a = 16.4f;
+            int b = 9;
+            float c;
+            c = a * (b + a) / (a - b);
+            Console.WriteLine(+c);
+            Console.ReadLine();
+
+
+            int h, i, j, x;
+            h = 90;
+            i = 15;
+            j= 3;
+            x = h - i / 3 + j * 2 - 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+            // Prints numbers from 1 to n
+            static void printNos(int n)
+            {
+                if (n > 0)
+                {
+                    printNos(n - 1);
+                    Console.Write(n + " ");
+                }
+                return;
+            }
+            printNos(10);
+            Console.ReadLine();
+
+        }
+    }
+}

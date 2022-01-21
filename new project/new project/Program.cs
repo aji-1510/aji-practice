06f07b5194cd929410bf3eaf1e882f98c1ce3c45
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-01-21 18:08:21 +0530

    Add files via upload

---
+++ new project/new project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace new_project
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            grt_3number obj = new grt_3number();
+            obj.GRT_demo();
+        }
+    }
+}
---
+++ new project/new project/grt_3number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_project
+{
+    class grt_3number
+    {
+        public void GRT_demo()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter three numbers");
+            num1=Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("Num1 is geraeter = {0}", num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("Num2 ius greater={0}", num2);
+            }
+            else
+            {
+                Console.WriteLine("Num3 is greater={0}",num3);
+            }
+
+        }
+
+    }
+}

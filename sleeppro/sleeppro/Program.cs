7155cd4780ab8d417bb7ee0aaf0db1dc22ae08e1
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-02-11 10:24:13 +0530

    Add files via upload

---
+++ sleeppro/sleeppro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+
+namespace sleeppro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char num2;
+            do
+            {
+                int num1;
+                
+                Console.WriteLine("sleep methoed program");
+                Console.WriteLine("which place sleep method proform 1 to 10");
+                num1 = int.Parse(Console.ReadLine());
+                sleep_demo(num1);
+                Console.WriteLine("you want run again this program Y/N");
+                num2 = Convert.ToChar(Console.ReadLine());
+
+            } while ((num2 == 'Y') || (num2 == 'y'));
+        }
+        public static void sleep_demo(int num1)
+        {
+            int i;
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("methoed value :{0}", i);
+                if (i==num1)
+                {
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+}

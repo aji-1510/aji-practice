e1061138e2e6c7ee00b1105dd631d329c4c31f2d
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-02-11 10:22:50 +0530

    Add files via upload

---
+++ threadpro/threadpro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace threadpro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread obj = new Thread(methoed1);
+            Thread obj1 = new Thread(methoed2);
+            obj.Start();
+            obj1.Start();
+
+            
+        }
+        public static void methoed1()
+        {
+            int i;
+/*            Console.WriteLine("methoed1");
+*/            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("methoed1 value:{0}", i);
+                if (i == 5)
+                {
+                    Thread.Sleep(5000);
+                }
+                
+            }
+
+        }
+        public static void methoed2()
+        {
+            int i;
+/*            Console.WriteLine("methoed2");
+*/            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("methoed2 value:{0}", i);
+                if (i == 5)
+                {
+                    Thread.Sleep(5000);
+                }
+
+            }
+
+        }
+
+    }
+}
